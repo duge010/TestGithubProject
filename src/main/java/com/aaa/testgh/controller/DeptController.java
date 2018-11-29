85202eebccc4cd8408e6a1c2de3553294b78c753
duge010 <Duge00000@163.com>
2018-11-29 09:32:48 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptTest
+ * discription:
+ * author:changdu
+ * createTime:2018/11/29 9:07
+ */
+public class DeptController {
+
+    public static void main(String[] args) {
+        System.out.println("测试GitHub提交11111111111");
+    }
+}
