7fd2c2ea15b9dee359bd5882213f8b6c18efe52e
duge010 <Duge00000@163.com>
2018-11-29 10:48:36 +0800

    在dev分支下加了打印

--- src/main/java/com/aaa/testgh/controller/DeptController.java
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -10,5 +10,7 @@ public class DeptController {
 
     public static void main(String[] args) {
         System.out.println("测试GitHub提交11111111111");
+        System.out.println("测试合并分支");
     }
+
 }
